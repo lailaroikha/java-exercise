cf19821be978aee1846956b1ed713b1ffcbb6ea5
Lailaroikha <lailajnnh@gmail.com>
2024-03-25 21:23:09 +0700

    exercise 4

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Scanner;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -9,6 +11,27 @@ public class Exercise4 {
      * @param args
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int number = scanner.nextInt();
+        System.out.println("Enter the number of the array: ");
+        int[] dataNumber = new int[number];
+        for (int i = 0; i < number; i++) {
+            dataNumber[i] = scanner.nextInt();
+        }
+        int length = dataNumber.length;
 
+        for (int i = 0; i < length; i++) {
+            boolean sameNumber = false;
+            for (int j = i + 1; j < length; j++) {
+                if (dataNumber[i] == dataNumber[j]) {
+                    sameNumber = true;
+                    break;
+                }
+            }
+            if (!sameNumber) {
+                System.out.print( dataNumber[i] + " ");
+            }
+        }
     }
 }
